72022d040637debc61687d7241414f8c8b2356d9
qureshiowaisaptachain <qureshi.owais@aptachain.com>
2023-08-19 16:44:55 +0530

    router added to app file

--- src/App.js
+++ src/App.js
@@ -8,18 +8,38 @@ import {
   RouterProvider,
 } from 'react-router-dom';
 import RootLayout from './components/pages/root';
-
+import ButtonComp from "./components/pages/button/button";
+import CardsComp from "./components/pages/cards/cards";
+import ColorComp from "./components/pages/color/color";
+import BorderComp from "./components/pages/borders/border";
+import OtherComp from "./components/pages/other/other";
+import Animation from "./components/pages/animation/animation";
+import RegisterComponent from "./components/pages/register/register";
+import ForgotPassword from "./components/pages/forgotPassoword/forgotpassword";
+import NotFound from "./components/pages/404/notfount"
 const routeDefination = createRoutesFromElements(
   <Route>
     <Route path='/' element={<LoginComp />} />
-    <Route path='/dashboard' element={<RootLayout />} />
+    <Route path='/login' element={<LoginComp />} />
+    <Route path='/register' element={<RegisterComponent/>}/>
+    <Route path='/forgot-password' element={<ForgotPassword/>}/>
+
+    <Route path='/dashboard' element={<RootLayout />}>
+      <Route path='/dashboard/buttons' element={<ButtonComp />} />
+      <Route path='/dashboard/cards' element={<CardsComp />} />
+      <Route path='/dashboard/utilities-color' element={<ColorComp />} />
+      <Route path='/dashboard/utilities-border' element={<BorderComp />} />
+      <Route path='/dashboard/utilities-animation' element={<Animation />} />
+      <Route path='/dashboard/utilities-other' element={<OtherComp />} />
+      <Route path='/dashboard/404' element={<NotFound/>}/>
+    </Route>
   </Route>
 );
 const router = createBrowserRouter(routeDefination);
 
 function App() {
   return (
-    <div className='App'>
+    <div className='App bg-gradient-primary'>
       <RouterProvider router={router} />
     </div>
   );
