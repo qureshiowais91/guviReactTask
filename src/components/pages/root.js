e5551bad86e5d6af7e5962cc0f46c53bd760d67e
qureshiowaisaptachain <qureshi.owais@aptachain.com>
2023-08-19 16:46:21 +0530

    route and ui fixes render sidebar route in root file

--- src/components/pages/root.js
+++ src/components/pages/root.js
@@ -1,9 +1,16 @@
-import Sidebar from "../sideBar";
-
-function RootLayout(){
-    return (<div>
-         <Sidebar></Sidebar>
-    </div>)
+import Sidebar from '../sideBar';
+import { Outlet } from 'react-router-dom';
+import Navbar from "../navbar/navbar";
+function RootLayout() {
+  return (
+    <div>
+      <Sidebar></Sidebar>
+      <Navbar></Navbar>
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
 }
 
 export default RootLayout;
