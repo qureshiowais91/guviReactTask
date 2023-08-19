c7d3b3b1927caf475d44eb221f3cc8c444d233f5
qureshiowaisaptachain <qureshi.owais@aptachain.com>
2023-08-19 16:45:41 +0530

    navbar UI fix

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,12 +1,11 @@
-import React from "react";
-import SearchBar from "./searchBar";
+import React from 'react';
+import SearchBar from './searchBar';
 const Navbar = () => {
   return (
-    <nav className="container-fluaid  navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-         <h1>
-          1111111111
-         </h1>
-         <SearchBar></SearchBar> 
+    <nav className='container-fluaid  navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow'>
+      <div className='container'>
+        <SearchBar></SearchBar>
+      </div>
     </nav>
   );
 };
